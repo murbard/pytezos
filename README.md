a15a70388013f90bfc5e6a63af72f7fdfb36b9b6
m-kus <44951260+m-kus@users.noreply.github.com>
2019-02-08 21:23:51 +0300

    Methods for generating and verifying signatures (#2)
    * Pep8 small edits [fix]
    Store private key instead of seed (ed25519 case) [fix]
    Return decoded strings, not bytes [imp]
    Sign and verify methods (ed25519, secp256k1 implemented) [feat]
    Unittests [imp]
    
    * Add readme description [imp]
    Add setup.py for installation [imp]
    
    * Migrate from pipfile to poetry, add travis ci
    
    * Standalone pysodium
    
    * Install libsodium before run
    
    * try apt addon
    
    * try export ld pathes
    
    * back to apt
    
    * try custom repo
    
    * Pass y flag
    
    * More badges!
    
    * Edit: python 3.6+ supported

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# pytezos
-Python utils for tezos
+# PyTezos
+
+[![Build Status](https://travis-ci.org/baking-bad/pytezos.svg?branch=master)](https://travis-ci.org/baking-bad/pytezos)
+[![Made With](https://img.shields.io/badge/made%20with-python-blue.svg?)](https://www.python.org)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Python utils for Tezos.
+
+## Requirements
+
+* git
+* python 3.6+
+* pip 19.0.1+
+
+## Installation
+
+```
+$ pip3 install git+https://github.com/baking-bad/pytezos --user
+```
+
+## Usage
+
+```python
+
+import pytezos
+
+private_key = 'edsk3nM41ygNfSxVU4w1uAW3G9EnTQEB5rjojeZedLTGmiGRcierVv'
+
+pytezos.Key(private_key).public_key()
+>>> 'edpku976gpuAD2bXyx1XGraeKuCo1gUZ3LAJcHM12W1ecxZwoiu22R'
+
+pytezos.Key(private_key).public_key_hash()
+>>> 'tz1eKkWU5hGtfLUiqNpucHrXymm83z3DG9Sq'
+
+pytezos.Key(private_key).sign('test')
+>>> 'edsigtzLBGCyadERX1QsYHKpwnxSxEYQeGLnJGsSkHEsyY8vB5GcNdnvzUZDdFevJK7YZQ2ujwVjvQZn62ahCEcy74AwtbA8HuN'
+
+public_key = 'edpku976gpuAD2bXyx1XGraeKuCo1gUZ3LAJcHM12W1ecxZwoiu22R'
+signature = 'edsigtzLBGCyadERX1QsYHKpwnxSxEYQeGLnJGsSkHEsyY8vB5GcNdnvzUZDdFevJK7YZQ2ujwVjvQZn62ahCEcy74AwtbA8HuN'
+
+pytezos.Key(public_key).verify(signature, 'fake')
+>>> Exception('Signature is not valid.')
+```
+
+## Supported elliptic curves
+
+* Ed25519 (tz1)
+* SECP256k1 (tz2)
+
+P256 signatures (tz3) will be implemented soon.
+
+## Troubleshooting
+
+* If you get ```command 'gcc' failed with exit status 1``` while installing secp256k1 package, it's probably system libraries issue: https://github.com/ludbb/secp256k1-py#installation-with-compilation
--- pytezos.py
+++ pytezos.py
@@ -2,31 +2,47 @@ import hashlib
 import pysodium
 from pyblake2 import blake2b
 import base58
-from base58 import scrub_input
 import secp256k1
 import unicodedata
 
+
 def tb(l):
-    return (b'').join(map(lambda x:x.to_bytes(1,'big'), l))
+    return b''.join(map(lambda x: x.to_bytes(1, 'big'), l))
+
+
+def scrub_input(v):
+    if isinstance(v, str) and not isinstance(v, bytes):
+        if v.startswith('0x'):
+            v = bytes.fromhex(v[2:])
+        else:
+            v = v.encode('ascii')
+
+    if not isinstance(v, bytes):
+        raise TypeError(
+            "a bytes-like object is required (also str), not '%s'" %
+            type(v).__name__)
+
+    return v
+
 
 b58_encodings = {
-    (b"tz1", 36):   (tb([6,161,159]),         u"ed25519 public key hash"),
-    (b"tz2", 36):   (tb([6,161,161]),         u"secp256k1 public key hash"),
-    (b"tz3", 36):   (tb([6,161,164]),         u"p256 public key hash"),
-    (b"edpk", 54):  (tb([13,15,37,217]),      u"ed25519 public key"),
-    (b"sppk", 55):  (tb([3,254,226,86]),      u"secp256k1 public key"),
-    (b"p2pk", 55):  (tb([3,178,139,127]),     u"p256 public key"),
-    (b"edsk", 54):  (tb([13,15,58,7]),        u"ed25519 seed"),
-    (b"spsk", 54):  (tb([17,162,224,201]),    u"secp256k1 secret key"),
-    (b"p2sk", 54):  (tb([16,81,238,189]),     u"p256 secret key"),
-    (b"edesk", 88): (tb([7,90,60,179,41]),    u"ed25519 encrypted seed"),
-    (b"spesk", 88): (tb([9,237,241,174,150]), u"secp256k1 encrypted secret key"),
-    (b"p2esk", 88): (tb([9,48,57,115,171]),   u"p256_encrypted_secret_key"),
-    (b"edsk", 98):  (tb([43,246,78,7]),       u"ed25519 secret key"),
-    (b"edsig", 99): (tb([9,245,205,134,18]),  u"ed25519 signature"),
-    (b"spsig", 99): (tb([13,115,101,19,63]),  u"secp256k1 signature"),
-    (b"p2sig", 98): (tb([54,240,44,52]),      u"p256 signature"),
-    (b"sig", 96):   (tb([4,130,43]),          u"generic signature")
+    (b"tz1",   36): (tb([6,  161, 159]),           u"ed25519 public key hash"),
+    (b"tz2",   36): (tb([6,  161, 161]),           u"secp256k1 public key hash"),
+    (b"tz3",   36): (tb([6,  161, 164]),           u"p256 public key hash"),
+    (b"edpk",  54): (tb([13, 15,  37,  217]),      u"ed25519 public key"),
+    (b"sppk",  55): (tb([3,  254, 226, 86]),       u"secp256k1 public key"),
+    (b"p2pk",  55): (tb([3,  178, 139, 127]),      u"p256 public key"),
+    (b"edsk",  54): (tb([13, 15,  58,  7]),        u"ed25519 seed"),
+    (b"spsk",  54): (tb([17, 162, 224, 201]),      u"secp256k1 secret key"),
+    (b"p2sk",  54): (tb([16, 81,  238, 189]),      u"p256 secret key"),
+    (b"edesk", 88): (tb([7,  90,  60,  179, 41]),  u"ed25519 encrypted seed"),
+    (b"spesk", 88): (tb([9,  237, 241, 174, 150]), u"secp256k1 encrypted secret key"),
+    (b"p2esk", 88): (tb([9,  48,  57,  115, 171]), u"p256_encrypted_secret_key"),
+    (b"edsk",  98): (tb([43, 246, 78,  7]),        u"ed25519 secret key"),
+    (b"edsig", 99): (tb([9,  245, 205, 134, 18]),  u"ed25519 signature"),
+    (b"spsig", 99): (tb([13, 115, 101, 19,  63]),  u"secp256k1 signature"),
+    (b"p2sig", 98): (tb([54, 240, 44,  52]),       u"p256 signature"),
+    (b"sig",   96): (tb([4,  130, 43]),            u"generic signature")
 }
 
 
@@ -46,23 +62,20 @@ class Key(object):
         if passphrase:
             passphrase = scrub_input(passphrase)
 
-        if not email is None:
+        if email is not None:
             mnemonic = u' '.join(key).lower()
             # TODO check wordlist and checksum
             salt = unicodedata.normalize(
                 "NFKD", (email + passphrase).decode("utf8")).encode("utf8")
 
             seed = hashlib.pbkdf2_hmac("sha512", mnemonic, "mnemonic" + salt, 2048, 64)
-            pk, sk = pysodium.crypto_sign_seed_keypair(seed[0:32])
-
+            self._public_key, self._secret_key = pysodium.crypto_sign_seed_keypair(seed[0:32])
             self.curve = b"ed"
-            self._secret_key = seed[0:32]
-            self._public_key = pk
             self.is_secret = True
             return
 
         self.curve = key[:2]  # "sp", "p2" "ed"
-        if not self.curve in [b"sp", b"p2", b"ed"]:
+        if self.curve not in [b"sp", b"p2", b"ed"]:
             raise Exception("Invalid prefix for a key encoding.")
 
         if not len(key) in [54, 55, 88, 98]:
@@ -101,9 +114,8 @@ class Key(object):
                 # Dealing with secret key or seed?
                 if encoding[1] == 98:
                     self._public_key = pysodium.crypto_sign_sk_to_pk(sk=key)
-                    self._secret_key = pysodium.crypto_sign_sk_to_seed(sk=key)
                 else:
-                    self._public_key = pysodium.crypto_sign_seed_keypair(seed=key)[0]
+                    self._public_key, self._secret_key = pysodium.crypto_sign_seed_keypair(seed=key)
             # Secp256k1
             elif self.curve == b"sp":
                 sk = secp256k1.PrivateKey(key)
@@ -114,31 +126,35 @@ class Key(object):
             else:
                 assert False
 
-
     def public_key(self):
         """
         :return: the public key associated with the private key
         """
         prefix = b58_encodings[(self.curve + b'pk', 54 if self.curve == b'ed' else 55)][0]
-        return base58.b58encode_check(prefix + self._public_key)
+        return base58.b58encode_check(prefix + self._public_key).decode()
 
     def secret_key(self, passphrase=None):
         """
+        :param passphrase: encryption phrase for the private key
         :return: the secret key associated with this key, if available
         """
-        if self._secret_key:
-            if passphrase:
-                prefix = b58_encodings[(self.curve + b'esk', 88)][0]
-                salt = pysodium.randombytes(8)
-                ek = hashlib.pbkdf2_hmac("sha512", passphrase, salt, 32768, 32)
-                key = pysodium.crypto_secretbox(
-                    msg=self._secret_key, nonce=b'\000' * 24, k=ek)
-            else:
-                prefix = b58_encodings[(self.curve + b'sk', 54)][0]
-                key = self._secret_key
-            return base58.b58encode_check(prefix + key)
-        else:
+        if not self._secret_key:
             raise Exception("Secret key not known.")
+
+        if self.curve == b'ed':
+            key = pysodium.crypto_sign_sk_to_seed(self._secret_key)
+        else:
+            key = self._secret_key
+
+        if passphrase:
+            prefix = b58_encodings[(self.curve + b'esk', 88)][0]
+            salt = pysodium.randombytes(8)
+            ek = hashlib.pbkdf2_hmac("sha512", passphrase, salt, 32768, 32)
+            key = pysodium.crypto_secretbox(msg=key, nonce=b'\000' * 24, k=ek)
+        else:
+            prefix = b58_encodings[(self.curve + b'sk', 54)][0]
+
+        return base58.b58encode_check(prefix + key).decode()
 
     def public_key_hash(self):
         """
@@ -147,4 +163,83 @@ class Key(object):
         """
         pkh = blake2b(data=self._public_key, digest_size=20).digest()
         prefix = b58_encodings[({b'ed': b'tz1', b'sp': b'tz2', b'p2': b'tz3'}[self.curve], 36)][0]
-        return base58.b58encode_check(prefix + pkh)
+        return base58.b58encode_check(prefix + pkh).decode()
+
+    def sign(self, message, generic=False):
+        """
+        Sign a raw sequence of bytes
+        :param message: sequence of bytes, raw format or hexadecimal notation
+        :param generic: do not specify elliptic curve if set to True
+        :return: signature in base58 encoding
+        """
+        message = scrub_input(message)
+
+        if not self.is_secret:
+            raise Exception("Cannot sign without a secret key.")
+
+        # Ed25519
+        if self.curve == b"ed":
+            digest = pysodium.crypto_generichash(message)
+            signature = pysodium.crypto_sign_detached(digest, self._secret_key)
+        # Secp256k1
+        elif self.curve == b"sp":
+            pk = secp256k1.PrivateKey(self._secret_key)
+            signature = pk.ecdsa_serialize_compact(
+                pk.ecdsa_sign(message, digest=lambda x: blake2b(x, digest_size=32)))
+        # P256
+        else:
+            raise NotImplementedError(self.curve)
+
+        if generic:
+            encoding = (b'sig', 96)
+        else:
+            encoding = (self.curve + b'sig', 98 if self.curve == b"p2" else 99)
+
+        prefix = b58_encodings[encoding][0]
+        return base58.b58encode_check(prefix + signature).decode()
+
+    def verify(self, signature, message):
+        """
+        Verify signature, raise exception if it is not valid
+        :param message: sequance of bytes, raw format or hexadecimal notation
+        :param signature: a signature in base58 encoding
+        """
+        signature = scrub_input(signature)
+        message = scrub_input(message)
+
+        if not self._public_key:
+            raise Exception("Cannot verify without a public key")
+
+        if signature[:3] == b'sig':
+            is_generic = True
+        else:
+            is_generic = False
+            if self.curve != signature[:2]:  # "sp", "p2" "ed"
+                raise Exception("Signature and public key curves mismatch.")
+
+        if not len(signature) in [99, 98, 96]:
+            raise Exception("Invalid length for a signature encoding.")
+
+        encoding = (signature[:3 if is_generic else 5], len(signature))
+        if encoding not in b58_encodings:
+            raise Exception("Invalid encoding for a signature, length or prefix mismatch.")
+
+        signature = base58.b58decode_check(signature)[len(b58_encodings[encoding][0]):]
+
+        # Ed25519
+        if self.curve == b"ed":
+            try:
+                digest = pysodium.crypto_generichash(message)
+                pysodium.crypto_sign_verify_detached(signature, digest, self._public_key)
+            except ValueError:
+                raise Exception('Signature is invalid.')
+        # Secp256k1
+        elif self.curve == b"sp":
+            pk = secp256k1.PublicKey(self._public_key, raw=True)
+            sig = pk.ecdsa_deserialize_compact(signature)
+            if not pk.ecdsa_verify(
+                    message, sig, digest=lambda x: blake2b(x, digest_size=32)):
+                raise Exception('Signature is invalid.')
+        # P256
+        else:
+            raise NotImplementedError(self.curve)
