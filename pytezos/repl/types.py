aab4a2466a3f515fc9e9f8b91328eda40b19faf7
m-kus <mz@baking-bad.org>
2020-01-22 11:15:43 +0300

    stash changes

---
+++ pytezos/repl/types.py
@@ -0,0 +1,24 @@
+class MichelsonType:
+    pass
+
+
+class String(MichelsonType):
+    pass
+
+
+class Nat(MichelsonType):
+    pass
+
+
+class Int(MichelsonType):
+    pass
+
+
+class Bytes(MichelsonType):
+    pass
+
+
+class Bool(MichelsonType):
+    pass
+
+
