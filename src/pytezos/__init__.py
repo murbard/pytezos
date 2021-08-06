48cd39662827320e7a76d00661a4c118b332b4bc
Michael Zaikin <mz@baking-bad.org>
2021-08-06 19:38:21 +0300

    Bump version: 3.2.6 → 3.2.7

--- src/pytezos/__init__.py
+++ src/pytezos/__init__.py
@@ -21,6 +21,6 @@ from pytezos.michelson.parse import michelson_to_micheline
 from pytezos.michelson.types.base import MichelsonType, Undefined
 from pytezos.michelson.types.core import Unit
 
-__version__ = '3.2.6'
+__version__ = '3.2.7'
 
 pytezos = PyTezosClient()
